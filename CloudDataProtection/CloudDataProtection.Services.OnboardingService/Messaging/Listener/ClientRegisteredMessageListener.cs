58fb61ccf971638716aa026cbfd26dc0e7df6aa4
Olivier Bouchoms <bouchoms.olivier@gmail.com>
2022-02-13 09:39:22 +0100

    Use direct exchange in RabbitMQ to correctly make use of routing keys

--- CloudDataProtection/CloudDataProtection.Services.OnboardingService/Messaging/Listener/ClientRegisteredMessageListener.cs
+++ CloudDataProtection/CloudDataProtection.Services.OnboardingService/Messaging/Listener/ClientRegisteredMessageListener.cs
@@ -4,6 +4,7 @@ using CloudDataProtection.Core.Messaging.RabbitMq;
 using CloudDataProtection.Services.Onboarding.Business;
 using CloudDataProtection.Services.Onboarding.Dto;
 using CloudDataProtection.Services.Onboarding.Entities;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -11,25 +12,27 @@ namespace CloudDataProtection.Services.Onboarding.Messaging.Listener
 {
     public class ClientRegisteredMessageListener : RabbitMqMessageListener<UserRegisteredModel>
     {
-        private readonly OnboardingBusinessLogic _logic;
+        private readonly IServiceScope _scope;
 
-        public ClientRegisteredMessageListener(IOptions<RabbitMqConfiguration> options, ILogger<ClientRegisteredMessageListener> logger, OnboardingBusinessLogic logic) :
+        public ClientRegisteredMessageListener(IOptions<RabbitMqConfiguration> options, ILogger<ClientRegisteredMessageListener> logger, IServiceScopeFactory serviceScopeFactory) :
             base(options, logger)
         {
-            _logic = logic;
+            _scope = serviceScopeFactory.CreateScope();
         }
 
         protected override string RoutingKey => RoutingKeys.ClientRegistered;
-        
+
         public override async Task HandleMessage(UserRegisteredModel model)
         {
+            OnboardingBusinessLogic logic = _scope.ServiceProvider.GetRequiredService<OnboardingBusinessLogic>();
+
             Entities.Onboarding onboarding = new Entities.Onboarding
             {
                 Status = OnboardingStatus.None,
                 UserId = model.Id
             };
 
-            await _logic.Create(onboarding);
+            await logic.Create(onboarding);
         }
     }
 }
